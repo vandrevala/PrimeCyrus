d2714714c2dbd6082294121871f0ad7b2808b810
Eric Meyer <emeyer@8thlight.com>
2022-02-19 13:35:51 -0600

    First commit

---
+++ PrimeCyrus/PrimeCyrusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeCyrusApp.swift
+//  PrimeCyrus
+//
+//  Created by Eric Meyer on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct PrimeCyrusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
